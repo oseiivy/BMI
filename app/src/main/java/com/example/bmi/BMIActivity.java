a344024896b2370692bcd42115d1988d818e4b19
Ivy Osei <oseiivy@my.yorku.ca>
2020-01-13 23:56:41 -0500

    BMI commit

--- app/src/main/java/com/example/bmi/BMIActivity.java
+++ app/src/main/java/com/example/bmi/BMIActivity.java
@@ -3,6 +3,9 @@ package com.example.bmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BMIActivity extends AppCompatActivity {
 
@@ -11,4 +14,18 @@ public class BMIActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bmi_layout);
     }
+
+    public void buttonClicked(View v)
+    {
+        EditText weightView = (EditText) findViewById(R.id.weightBox);
+        String weight = weightView.getText().toString();
+        EditText heightView = (EditText) findViewById(R.id.heightBox);
+        String height = heightView.getText().toString();
+
+        BMIModel myModel = new BMIModel(weight, height);
+        String myBMI = myModel.getBMI();
+
+        ((TextView) findViewById(R.id.answer)).setText(myBMI);
+    }
+
 }
